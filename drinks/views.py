0257cc21223da56d6058b1c73f3951395b5b6c02
ttwange <ttwange2@gmail.com>
2023-06-22 04:38:40 +0300

    working on the serializers

--- drinks/views.py
+++ drinks/views.py
@@ -2,8 +2,10 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
 
 
+@api_view('GET','POST')
 def drink_list(request):
     drinks = Drink.objects.all()
     serializer = DrinkSerializer(drinks, many=True)
