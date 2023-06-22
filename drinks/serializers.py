169ea565d7f053c76db7483b623fc101858f8fe6
ttwange <ttwange2@gmail.com>
2023-06-22 04:31:23 +0300

    working on the serializers

---
+++ drinks/serializers.py
@@ -0,0 +1,9 @@
+#explains the process from python object to json
+
+from rest_framework import serializers
+from .models import Drink
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ('id', 'name','description')
