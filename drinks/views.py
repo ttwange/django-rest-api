f21c8c86040cf4c8e706ab7ff866d7c549f20018
ttwange <ttwange2@gmail.com>
2023-06-22 04:35:25 +0300

    working on the serializers

--- drinks/views.py
+++ drinks/views.py
@@ -7,4 +7,4 @@ from .serializers import DrinkSerializer
 def drink_list(request):
     drinks = Drink.objects.all()
     serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse({'drinks':serializer.data}, safe=False)
+    return JsonResponse({'drinks':serializer.data})
