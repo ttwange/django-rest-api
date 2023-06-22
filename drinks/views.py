3a80eee12f967861a38b2674a1d0c20e02dc8ad1
ttwange <ttwange2@gmail.com>
2023-06-22 04:59:49 +0300

    working on the serializers

--- drinks/views.py
+++ drinks/views.py
@@ -1,12 +1,22 @@
 
+from rest_framework.response import Response
+from rest_framework import status
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
 
 
-@api_view('GET','POST')
+@api_view(['GET','POST'])
 def drink_list(request):
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse({'drinks':serializer.data})
+    
+    if request.method == 'GET':
+      drinks = Drink.objects.all()
+      serializer = DrinkSerializer(drinks, many=True)
+      return JsonResponse({'drinks':serializer.data})
+    
+    if request.method == 'POST':
+       serializer = DrinkSerializer(data=request.data)
+       if serializer.is_valid():
+          serializer.save()
+          return Response(serializer.data, status=status.HTTP_201_CREATED)
