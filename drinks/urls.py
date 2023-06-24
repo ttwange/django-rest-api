d0a6d8ef2a4b4e9a261329d218a8ada268752bcc
ttwange <ttwange2@gmail.com>
2023-06-24 09:49:11 +0300

    changes to the views  files

--- drinks/urls.py
+++ drinks/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from drinks import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('drinks/', views.drink_list),
-    path('drinks/<int:id>',views.drink_detail)
+    path('drinks/<int:id>', views.drink_detail)
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- drinks/views.py
+++ drinks/views.py
@@ -8,12 +8,12 @@ from rest_framework.decorators import api_view
 
 
 @api_view(['GET','POST'])
-def drink_list(request):
+def drink_list(request,format=None):
     
     if request.method == 'GET':
       drinks = Drink.objects.all()
       serializer = DrinkSerializer(drinks, many=True)
-      return JsonResponse({'drinks':serializer.data})
+      return Response(serializer.data)
     
     if request.method == 'POST':
        serializer = DrinkSerializer(data=request.data)
@@ -22,7 +22,7 @@ def drink_list(request):
           return Response(serializer.data, status=status.HTTP_201_CREATED)
        
 @api_view(['GET','PUT','DELETE'])
-def drink_detail(request,id):
+def drink_detail(request, id, format=None):
    try:
       drink = DrinkSerializer.objects.get(pk=id)
    except Drink.DoesNotExist:
